bf30a58963f75ac9086637f5d637fdfc4bf5470d
magpantaynic <20709370+magpantaynic@users.noreply.github.com>
2022-03-25 16:11:30 -0400

    Create DoItBetter.java

---
+++ DoItBetter.java
@@ -0,0 +1,19 @@
+/* 
+  Ask the user how many miles they can run, the number of hot dogs they can eat, and the number of languages   they know. Immediately after each question, brag that you can do more by multiplying their number by 2 and    adding 1.
+*/
+
+import java.util.Scanner;
+
+public class DoItBetter {
+
+  public static void main(String[] args) {
+
+      Scanner inputReader = new Scanner(System.in);
+
+    
+      
+    
+    }
+
+  
+}
