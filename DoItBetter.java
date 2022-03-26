612f98e9598b6feab3a0c43059ae1ee14bd16505
Nicole Magpantay <20709370+magpantaynic@users.noreply.github.com>
2022-03-26 13:45:06 -0400

    Update DoItBetter.java

--- DoItBetter.java
+++ DoItBetter.java
@@ -1,5 +1,5 @@
 /* 
-  Ask the user how many miles they can run, the number of hot dogs they can eat, and the number of languages   they know. Immediately after each question, brag that you can do more by multiplying their number by 2 and    adding 1.
+  Ask the user how many miles they can run, the number of hot dogs they can eat, and the number of languages they know. Immediately after each question, brag that you can do more by multiplying their number by 2 and adding 1.
 */
 
 import java.util.Scanner;
@@ -10,8 +10,19 @@ public class DoItBetter {
 
       Scanner inputReader = new Scanner(System.in);
 
+      int milesUser;
+      int hotDogs;
+      int langUser;
     
-      
+      System.out.println("How many miles can you run?");
+      // milesUser = inputReader.nextLine();
+      System.out.println("How many hot dogs can you eat?");
+      // hotDogs = inputReader.nextLine();
+      System.out.println("How many languages can you speak?");
+      // langUser = inputReader.nextLine();
+
+    // *2 + 1
+    
     
     }
 
